6604828a0bacc7f6175c0bd85fc6cce8d1400869
lance <lancee478@gmail.com>
2021-06-18 13:16:17 -0700

    menu shows alive/dead bot state, removed unused imports

--- server.py
+++ server.py
@@ -3,10 +3,7 @@ import socket, pickle, security
 import string
 import threading, traceback
 import time
-from consolemenu import *
-from consolemenu.items import *
 from datetime import datetime
-from copy import deepcopy
 
 addr = ('127.0.0.1', 5000)
 decryptor = security.get_decryptor()
@@ -52,7 +49,6 @@ class User():
                 self.conn[0].send(pickle.dumps('keepalive'))
                 time.sleep(3)
             except:
-
                 self.hist.append({'from': 'server', 'msg': 'client disconnected', 'timestamp': datetime.now().strftime('%m-%d-%Y %I:%M:%S%p')})
                 self.alive = False
                 return
@@ -97,30 +93,31 @@ def handle_user(user):
 
 def get_instructions(): #todo add file down/uploading from bot, add more options
     opt = get_choice(['Select a bot'], 'Main Menu')
-    #opt = SelectionMenu.get_selection(['Select a bot'], 'Main Menu')
     if opt == 0:
         curr_users = [u.id for u in users]  # in case list of users changes before selection made
-        opt = get_choice([f'{user.ip}:{user.port}' for user in users] + ['Main Menu'], 'Select a bot:' if len(users) > 0 else 'Select a bot:\nNo bots found!!!')
-        #opt = SelectionMenu.get_selection([f'{user.ip}:{user.port}' for user in users] + ['Main Menu'], 'Select a bot:', subtitle='No bots found!!' if len(users) <= 0 else None)
+        opt = get_choice([f'{user.ip}:{user.port} | {"ALIVE" if user.alive else "DEAD"}' for user in users] + ['Main Menu'], 'Select a bot:' if len(users) > 0 else 'Select a bot:\nNo bots found!!!')
         if opt == len(curr_users):
             get_instructions()
         else:
             selected_user = curr_users[opt] #in case users list indexes change, can still get intented user using get_user(selected_user.id)
-            bot_opts = ['Send a command', 'View bot session logs', 'Main Menu']
-            opt = get_choice(bot_opts, 'Choose an action:')
-            #opt = SelectionMenu.get_selection(bot_opts, 'Choose an action:')
-            if opt == len(bot_opts) - 1:
-                get_instructions()
-            elif opt == 0:
-                cmd = input('Enter a command: ')
-                confirm = input(f'Are you sure you would like to run "{cmd}" on {get_user(selected_user).ip}:{get_user(selected_user).port}? (y/n) ')
-                if confirm.lower().startswith('y'):
-                    print(f'Running command...')
-                    print('TODO make command actually run') #TODO make command actually run
-                get_instructions()
-            elif opt == 1:
-                for msg in get_user(selected_user).hist: #todo just give last 100 lines? add option for full?
-                    print(f'{msg["timestamp"]} | {msg["from"]}: {msg["msg"]}')
+            if get_user(selected_user).alive:
+                bot_opts = ['Send a command', 'View bot session logs', 'Main Menu']
+                opt = get_choice(bot_opts, 'Choose an action:')
+                if opt == len(bot_opts) - 1:
+                    get_instructions()
+                elif opt == 0:
+                    cmd = input('Enter a command: ')
+                    confirm = input(f'Are you sure you would like to run "{cmd}" on {get_user(selected_user).ip}:{get_user(selected_user).port}? (y/n) ')
+                    if confirm.lower().startswith('y'):
+                        print(f'Running command...')
+                        print('TODO make command actually run') #TODO make command actually run
+                    get_instructions()
+                elif opt == 1:
+                    for msg in get_user(selected_user).hist: #todo just give last 100 lines? add option for full?
+                        print(f'{msg["timestamp"]} | {msg["from"]}: {msg["msg"]}')
+                    get_instructions()
+            else:
+                print('Dead bot, chose a live one')
                 get_instructions()
 
 def run():
